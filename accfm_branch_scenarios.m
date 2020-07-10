6fe82d88217c0f7c74e83651fcd8c5aad42149a5
mnoebels <58218146+mnoebels@users.noreply.github.com>
2020-07-10 12:14:14 +0100

    files for batch processing

---
+++ accfm_branch_scenarios.m
@@ -0,0 +1,150 @@
+function result = accfm_branch_scenarios(network, scenarios, settings)
+% running AC-CFM for a set of initial contingencies
+%   accfm_branch_scenarios(network, scenarios, settings) runs the AC-CFM in
+%   network with for the initial contingencies given in scenarios.
+%   result_cascade = accfm_branch_scenarios( ___ ) returns a struct
+%   including the individual results for every scenario.
+
+    % use default settings if not defined
+    if ~exist('settings', 'var')
+        settings = get_default_settings();
+    end
+
+    %% initialise variables
+    number_of_scenarios = length(scenarios);
+
+    result.scenarios = scenarios;
+    contingency = zeros(number_of_scenarios, 1);
+    lost_load_final = zeros(number_of_scenarios, 1);
+    
+    tripped_buses_in_generation = zeros(number_of_scenarios, settings.max_iterations);
+    tripped_buses_in_scenario = zeros(number_of_scenarios, size(network.bus, 1), 'logical');
+    
+    tripped_lines_in_generation = zeros(number_of_scenarios, settings.max_iterations);
+    tripped_lines_in_scenario = zeros(number_of_scenarios, size(network.branch, 1), 'logical');
+    line_criticality = zeros(size(network.branch, 1), 1);
+    
+    ufls_buses = zeros(number_of_scenarios, size(network.bus, 1), 'logical');
+    uvls_buses = zeros(number_of_scenarios, size(network.bus, 1), 'logical');
+    tripped_gens_in_generation = zeros(number_of_scenarios, settings.max_iterations);
+    tripped_gens_in_scenario = zeros(number_of_scenarios, size(network.gen, 1), 'logical');
+    
+    lost_load = zeros(number_of_scenarios, settings.max_iterations);
+    ls_ufls = zeros(number_of_scenarios, 1);
+    ls_uvls = zeros(number_of_scenarios, 1);
+    ls_vcls = zeros(number_of_scenarios, 1);
+    ls_opf = zeros(number_of_scenarios, 1);
+    ls_tripped = zeros(number_of_scenarios, 1);
+    load_at_time = cell(number_of_scenarios, 1);
+    
+    computational = zeros(number_of_scenarios, 2); 
+    vcls = zeros(number_of_scenarios, 6);
+    
+    result.network = network;
+    result.settings = settings;
+    
+    %% the loop
+    % output progress if not running on cluster
+    startTime = tic;
+    if ~isdeployed
+        fprintf('\t Completion: ');
+        showTimeToCompletion;
+        p = parfor_progress( number_of_scenarios );
+    end
+    
+    % use parallel computing toolbox
+    parfor i = 1:number_of_scenarios
+        %output progress if not running on cluster
+        if ~isdeployed
+            p = parfor_progress;
+            showTimeToCompletion( p/100, [], [], startTime );
+        else
+            fprintf('Scenario %d', i);
+        end
+        
+        % AC-CFM only produces an error if iteration limit is reached
+        try
+            % run the model
+            result_cascade = accfm(network, struct('branches', scenarios{i}), settings);
+
+            % get results and store them in the result arrays
+            contingency(i) = length(scenarios{i});
+            tripped_lines_in_generation(i, :) = sum(result_cascade.branch_tripped);
+            tripped_lines_in_scenario(i, :) = sum(result_cascade.branch_tripped, 2);
+            line_criticality = line_criticality + sum(result_cascade.branch_tripped, 2);
+            
+            tripped_buses_in_generation(i, :) = sum(result_cascade.bus_tripped);
+            tripped_buses_in_scenario(i, :) = sum(result_cascade.bus_tripped, 2);
+            
+            ufls_buses_i = zeros(size(result_cascade.bus, 1), 1);
+            ufls_buses_i(sum(result_cascade.bus_ufls, 2) > 0) = 1;
+            ufls_buses(i, :) = ufls_buses_i;
+            
+            uvls_buses_i = zeros(size(result_cascade.bus, 1), 1);
+            uvls_buses_i(sum(result_cascade.bus_uvls, 2) > 0) = 1;
+            uvls_buses(i, :) = uvls_buses_i;
+            
+            tripped_gens_in_generation(i, :) = sum(result_cascade.gen_tripped);
+            tripped_gens_in_scenario(i, :) = sum(result_cascade.gen_tripped, 2);
+            
+            ls_i = 1 - result_cascade.load / sum(network.bus(:, 3));
+            lost_load(i, :) = ls_i;
+            
+            ls_ufls(i) = result_cascade.ls_ufls;
+            ls_uvls(i) = result_cascade.ls_uvls;
+            ls_vcls(i) = result_cascade.ls_vcls;
+            ls_opf(i) = result_cascade.ls_opf;
+            ls_tripped(i) = result_cascade.ls_tripped;
+            lost_load_final(i) = ls_i(end);
+            
+            rng(100000 + i);
+            load_at_time{i} = get_load_vs_time(result_cascade);
+            
+            vc_edges = find(strcmp(result_cascade.G.Edges.Type, 'VC'));
+            vc_nodes = findnode(result_cascade.G, result_cascade.G.Edges.EndNodes(vc_edges, 2));
+            vc_island_sizes = result_cascade.G.Nodes.Buses(vc_nodes);
+
+            opf_edges = find(strcmp(result_cascade.G.Edges.Type, 'OPF'));
+            opf_nodes = findnode(result_cascade.G, result_cascade.G.Edges.EndNodes(opf_edges, 2));
+            opf_island_sizes = result_cascade.G.Nodes.Buses(opf_nodes);
+
+            dc_edges = find(strcmp(result_cascade.G.Edges.Type, 'DC'));
+            dc_lines = result_cascade.G.Edges.Weight(dc_edges);
+            
+            computational(i, :) = [result_cascade.pf_count result_cascade.elapsed];
+            vcls(i, :) = [length(vc_edges) mean(vc_island_sizes) length(opf_edges) mean(opf_island_sizes) length(dc_edges) mean(dc_lines)];
+
+            if isdeployed
+                fprintf(' (%.2f%%)\n', ls_i(end) * 100);
+            end
+        catch
+            % iteration limit reached
+            
+            lost_load_final(i) = -1;
+        end
+    end
+    
+    toc(startTime);
+    
+    result.contingency = contingency;
+    result.tripped_lines_in_scenario = tripped_lines_in_scenario;
+    result.tripped_lines_in_generation = tripped_lines_in_generation;
+    result.line_criticality = line_criticality;
+    
+    result.tripped_buses_in_scenario = tripped_buses_in_scenario;
+    result.tripped_buses_in_generation = tripped_buses_in_generation;
+    result.ufls_buses = ufls_buses;
+    result.uvls_buses = uvls_buses;
+    result.tripped_gens_in_scenario = tripped_gens_in_scenario;
+    result.tripped_gens_in_generation = tripped_gens_in_generation;
+    result.lost_load = lost_load;
+    result.ls_ufls = ls_ufls;
+    result.ls_uvls = ls_uvls;
+    result.ls_vcls = ls_vcls;
+    result.ls_opf = ls_opf;
+    result.ls_tripped = ls_tripped;
+    result.lost_load_final = lost_load_final;
+    result.load_at_time = load_at_time;
+    result.computational = computational;
+    result.vcls = vcls;
+end
---
+++ accfm_pdf_batch.m
@@ -0,0 +1,61 @@
+function accfm_pdf_batch(network, pdf, alpha, number_of_scenarios, output_file)
+% batch processing for the AC Cascading Fault Model following a probability
+% distribution
+%   accfm_pdf_batch(network, pdf, alpha, number_of_scenarios, output_file) 
+%   runs the AC-CFM in network, which can either be a matpower case struct
+%   or a filename of a matpower case file for number_of_scenarios scenarios
+%   with the initial contingency size following a probability distribution
+%   specified in pdf and defined by parameter alpha. Currently, only 'zipf'
+%   can be used as a pdf. The results are stored in the file specified in
+%   output_file.
+
+    % load the network if it is given as a file
+    if (isstring(network) || ischar(network)) && isfile(network)
+        variableInfo = who('-file', network);
+        if ismember('network', variableInfo)
+            load(network, 'network');
+        else
+            [network, info] = loadcase(network);
+            if ~info
+                error("Invalid network case file");
+            end
+        end
+    elseif (isstring(network) || ischar(network)) && ~isfile(network)
+        error("Network file not found");
+    end
+
+    % convert non-numeric input parameters
+	if ~isnumeric(alpha)
+        alpha = str2double(alpha);
+    end
+    if ~isnumeric(number_of_scenarios)
+        number_of_scenarios = str2double(number_of_scenarios);
+    end
+	
+    % create the initial contingecies
+	rng('shuffle');
+	if strcmp(pdf, 'zipf')
+		contingencies = zipfrnd(alpha, [number_of_scenarios 1]);
+	end
+
+	scenarios = cell(number_of_scenarios, 1);
+	for i = 1:number_of_scenarios
+		scenarios{i} = round(rand(contingencies(i), 1) * size(network.branch, 1));
+	end
+
+	% model settings
+	settings = get_default_settings();
+	settings.max_iterations = 100;
+	settings.DC_fallback = 0;
+    
+    network = runpf(network, settings.mpopt);
+    
+    if ~network.success
+        error("Initial PF of network failed, please check network case");
+    end
+
+	% run AC-CFM
+	result = accfm_branch_scenarios(network, scenarios, settings);
+
+	% save result struct
+	save(output_file, 'result');
