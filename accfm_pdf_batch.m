fdc9774441a6d7babae8d5cf488fff74d2947dde
mnoebels <58218146+mnoebels@users.noreply.github.com>
2020-09-28 14:57:22 +0100

    Update accfm_pdf_batch.m
    option to pass multiple network structs

--- accfm_pdf_batch.m
+++ accfm_pdf_batch.m
@@ -20,12 +20,14 @@ function result = accfm_pdf_batch(network, pdf, alpha, number_of_scenarios, sett
                 error("Invalid network case file");
             end
         end
+    elseif iscell(network)
+        number_of_scenarios = length(network);
     elseif (isstring(network) || ischar(network)) && ~isfile(network)
         error("Network file not found");
     end
 
     % convert non-numeric input parameters
-	if ~isnumeric(alpha)
+    if ~isnumeric(alpha)
         alpha = str2double(alpha);
     end
     if ~isnumeric(number_of_scenarios)
@@ -45,17 +47,53 @@ function result = accfm_pdf_batch(network, pdf, alpha, number_of_scenarios, sett
 
 	scenarios = cell(number_of_scenarios, 1);
     for i = 1:number_of_scenarios
-		scenarios{i} = round(rand(contingencies(i), 1) * size(network.branch, 1));
+        if iscell(network)
+            n_branch = size(network{i}.branch, 1);
+        else
+            n_branch = size(network.branch, 1);
+        end
+        
+		scenarios{i} = round(rand(contingencies(i), 1) * n_branch);
     end
     
-    % run an initial PF to see if the case is valid
-    network = runpf(network, settings.mpopt);
-    if ~network.success
-        error("Initial PF of network failed, please check network case");
-    end
+    if ~iscell(network)
+        % run an initial PF to see if the case is valid
+        network = runpf(network, settings.mpopt);
+        if ~network.success
+            error("Initial PF of network failed, please check network case");
+        end
 
-	% run AC-CFM
-	result = accfm_branch_scenarios(network, scenarios, settings);
+        % run AC-CFM
+        result = accfm_branch_scenarios(network, scenarios, settings);
+    else
+        % output progress if not running on cluster
+        startTime = tic;
+        if ~isdeployed
+            fprintf('\t Completion: ');
+            showTimeToCompletion;
+            p = parfor_progress( number_of_scenarios );
+        end
+    
+        result(number_of_scenarios) = struct('version', [], 'baseMVA', [], 'bus', [], 'gen', [], 'branch', [], 'gencost', [], 'gentype', [], 'genfuel', [], 'bus_name', [], 'order', [], 'et', [], 'success', [], 'iterations', [], 'bus_id', [], 'gen_id', [], 'branch_id', [], 'branch_tripped', [], 'bus_tripped', [], 'bus_uvls', [], 'bus_ufls', [], 'gen_tripped', [], 'load', [], 'generation_before', [], 'pf_count', [], 'G', [], 'ls_total', [], 'ls_ufls', [], 'ls_uvls', [], 'ls_vcls', [], 'ls_opf', [], 'ls_tripped', [], 'elapsed', []);
+        
+        parfor i = 1:number_of_scenarios
+            %output progress if not running on cluster
+            if ~isdeployed
+                p = parfor_progress;
+                showTimeToCompletion( p/100, [], [], startTime );
+            else
+                fprintf('Scenario %d', i);
+            end
+            
+            network_i = runpf(network{i}, settings.mpopt);
+            if ~network_i.success
+                error("Initial PF of network failed, please check network case");
+            end
+            
+            % run AC-CFM
+            result(i) = accfm(network_i, struct('branches', scenarios{i}), settings);
+        end
+    end
 
     if exist('output_file', 'var')
         % save result struct
