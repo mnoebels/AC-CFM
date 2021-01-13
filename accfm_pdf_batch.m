2881ee3fe651cf6fc72a9f3bb530bb4b01d258c8
mnoebels <58218146+mnoebels@users.noreply.github.com>
2021-01-13 10:07:43 +0000

    fixed random sampling bug

--- accfm_pdf_batch.m
+++ accfm_pdf_batch.m
@@ -56,7 +56,11 @@ function result = accfm_pdf_batch(network, distribName, distribParams, number_of
             n_branch = size(network.branch, 1);
         end
         
-		scenarios{i} = round(rand(contingencies(i), 1) * n_branch);
+        if contingencies{i} > n_branch
+            contingencies{i} = n_branch;
+        end
+        
+		scenarios{i} = randperm(n_branch, contingencies(i));
     end
     
     if ~iscell(network)
